2c011fd176dcd2d6eaf338d20f27ce95aee25915
Troy Martz <troy@offgridpro.com>
2019-04-08 13:40:18 -0500

    Initial copy / paste from Katana mock.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,266 @@
 <template>
-  <div>
-    <Splash />
-    <IconList :itemlist="services" />
-    <TextShow :text="mission" />
-    <Team :members="team" />
-    <IconList :itemlist="portfolio" />
-    <TextShow :text="fees" />
-  </div>
+    <!-- Start: App -->
+
+    <div id="app">
+      <!-- Start: Navigation -->
+
+      <header id="nav">
+        <section>
+          <nav>
+            <ul>
+              <li><a id="introBtn" href="#intro">Katana</a></li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li><a id="missionBtn" href="#mission">Mission</a></li>
+              <li><a id="servicesBtn" href="#services">Services</a></li>
+              <li><a id="teamBtn" href="#team">Team</a></li>
+              <li><a id="portfolioBtn" href="#portfolio">Portfolio</a></li>
+              <li><a id="engageBtn" href="#engage">Engage</a></li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li><a id="contactBtn" href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+        </section>
+      </header>
+
+      <!-- End: Navigation | Start: Main -->
+
+      <main id="main">
+        <!-- Start: Intro -->
+
+        <section id="intro">
+          <div>
+            <div>
+              <svg
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 760 200"
+              >
+                <style>
+                  .st0 {
+                    fill: #fff;
+                  }
+                </style>
+                <path
+                  class="st0"
+                  d="M469.86 125.52v44.41l-13.35-45.58-99.64-8.72-15.69 56.08h37.04l6.9-27.73h38.38l7.63 27.73h73.2V141.1c0-4.21 0-8.41-.01-12.57l-34.46-3.01zM594.84 136.45l-9.41 33.64v-34.46l-60.89-5.33c4.52 11.19 10.04 24.8 16.81 41.41h80.66l6.9-27.73h38.38l7.63 27.73h39.25l-7.46-25.47-111.87-9.79zM291.85 109.94v61.77h37.83v-58.46zM224.9 104.09c.9 3.45 1.82 6.94 2.77 10.47H201.4c1.07-4.09 2.12-8.18 3.14-12.25l-32.74-2.86-15.04 53.75-32.06-57.88-42.23-3.69c-.26.38-.52.77-.78 1.15v-1.22l-36.96-3.23v83.39h37.09V130.8l13.82-16.59 26.96 57.5h66l6.9-27.73h38.38l7.63 27.73h39.25l-18.85-64.38-37.01-3.24z"
+                />
+                <g>
+                  <path
+                    class="st0"
+                    d="M714.72 139.24L682.08 27.77H631.3l-28.45 101.68 111.87 9.79zM655.37 53.1h.55c2.85 14.11 7.61 33.86 13.16 54.45H642.8c5.12-19.53 9.79-39.38 12.57-54.45zM464.51 117.35l-26.23-89.58H387.5l-22.63 80.86 99.64 8.72zM411.57 53.1h.55c2.85 14.11 7.61 33.86 13.16 54.45H399c5.12-19.53 9.79-39.38 12.57-54.45zM512.33 121.53c-.05-19.26-.26-37.63-1.26-51.78h.41c1.36 4.04 4.48 12.52 21.06 53.55l60.89 5.33V27.77h-34.55v30.11c0 21.28.48 44.73 1.88 60.74h-.68c-1.65-5.76-7.56-21.54-36.53-90.85h-45.69v90.75l34.47 3.01z"
+                  />
+                  <g>
+                    <path
+                      class="st0"
+                      d="M212.54 95.3c3.85-15.38 7.23-30.29 9.43-42.2h.55c2.36 11.7 6.04 27.27 10.39 43.99l37.02 3.24-21.25-72.55H197.9l-18.1 64.67 32.74 2.85zM132.7 88.32l-3.26-5.89 43.28-54.66h-44.14c-10.6 15.79-25.42 38.02-38.11 56.85l42.23 3.7zM89.69 84.56V27.77H52.73v53.55z"
+                    />
+                  </g>
+                  <path
+                    class="st0"
+                    d="M299.85 102.94l37.83 3.31V56.28h35.14V27.77H264.5v28.51h35.35z"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+        </section>
+
+        <!-- End: Intro | Start: Mission -->
+
+        <section id="mission">
+          <div>
+            <div>
+              <h2>The Katana Mission</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam
+                arcu, ornare vel ante non, imperdiet scelerisque turpis. Nulla
+                at enim nec erat pulvinar commodo. Maecenas eu efficitur enim,
+                eget malesuada mauris. Donec dictum odio dui, ut posuere dolor
+                aliquam quis. Vivamus at sagittis mi. Fusce nec massa lacinia,
+                sodales felis ac, aliquam orci. In commodo efficitur neque. Sed
+                sit amet maximus nulla. Morbi in scelerisque lorem, suscipit
+                fermentum sem. Etiam id ante sit amet ipsum accumsan cursus quis
+                nec turpis.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <!-- End: Mission | Start: Services -->
+
+        <section id="services">
+          <div>
+            <div>
+              <h2>Expert Services</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam
+                arcu, ornare vel ante non, imperdiet scelerisque turpis. Nulla
+                at enim nec erat pulvinar commodo. Maecenas eu efficitur enim,
+                eget malesuada mauris. Donec dictum odio dui, ut posuere dolor
+                aliquam quis. Vivamus at sagittis mi. Fusce nec massa lacinia,
+                sodales felis ac, aliquam orci. In commodo efficitur neque. Sed
+                sit amet maximus nulla. Morbi in scelerisque lorem, suscipit
+                fermentum sem. Etiam id ante sit amet ipsum accumsan cursus quis
+                nec turpis.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <!-- End: Services | Start: Team -->
+
+        <section id="team">
+          <div>
+            <div>
+              <h2>Team</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam
+                arcu, ornare vel ante non, imperdiet scelerisque turpis. Nulla
+                at enim nec erat pulvinar commodo.
+              </p>
+            </div>
+            <section>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>Sam Bourque</h3>
+                  <h4>Director</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>Troy Martz</h3>
+                  <h4>Designer</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>Yuri Nagano</h3>
+                  <h4>Accountant</h4>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        <!-- End: Team | Start: Portfolio -->
+
+        <section id="portfolio">
+          <div>
+            <div>
+              <h2>Portfolio</h2>
+            </div>
+            <section>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>AuditPM</h3>
+                  <h4>Property Management</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>TZ Ninja</h3>
+                  <h4>Timezone Management</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <figure>
+                    <img src="" alt="" />
+                  </figure>
+                  <h3>Cryptique</h3>
+                  <h4>Cryptocurrency Arbitrage</h4>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        <!-- End: Portfolio | Start: Engage -->
+
+        <section id="engage">
+          <div>
+            <div>
+              <h2>Let's Do Business!</h2>
+              <p>
+                Donec efficitur dolor orci, sed mattis orci aliquam quis.
+                Maecenas porttitor convallis purus, nec luctus ligula
+                ullamcorper sit amet. Pellentesque ut ipsum accumsan, volutpat
+                turpis non, placerat sem. Quisque maximus at lorem id faucibus.
+                Praesent pretium tincidunt dui id consequat.
+              </p>
+              <p>
+                Phasellus volutpat velit at justo lacinia tempor. Etiam pulvinar
+                eros sed convallis malesuada. Mauris est ipsum, tempus non lacus
+                non, varius ultricies odio. Donec nulla ex, sodales sit amet
+                tortor nec, tristique bibendum risus. Mauris imperdiet leo non
+                libero ullamcorper hendrerit. Duis blandit pulvinar ante ac
+                condimentum.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <!-- End: Engage -->
+      </main>
+
+      <!-- End: Main | Start: Footer -->
+
+      <footer id="footer">
+        <section>
+          <div>
+            <div>
+              <p>
+                &copy; 2019 Katana Development Services. All Rights Reserved.
+              </p>
+            </div>
+          </div>
+        </section>
+      </footer>
+
+      <!-- End: Footer | Begin: Navigator -->
+
+      <!--    
+      <aside id="navigator">
+        <section>
+          <nav>
+            <ul>
+              <li><a href="#">help</a></li>
+              <li><a href="#">top</a></li>
+              <li><a href="#">previous</a></li>
+              <li><a href="#">next</a></li>
+            </ul>
+          </nav>
+        </section>
+      </aside>
+      -->
+    </div>
+
+    <!-- End: App -->
 </template>
+
+
+
 
 <script>
 import Splash from '@/components/Splash'
