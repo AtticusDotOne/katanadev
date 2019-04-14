0e208369a8553b6c9d261e5face2174c2821eac6
Troy Martz <troy@offgridpro.com>
2019-04-14 12:13:00 -0500

    Major integration tweaks

---
+++ static/js/scroll.js
@@ -0,0 +1,14 @@
+$("html").easeScroll({
+  frameRate: 60,
+  animationTime: 750,
+  stepSize: 90,
+  pulseAlgorithm: 1,
+  pulseScale: 8,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: true,
+  arrowScroll: 50,
+  touchpadSupport: true,
+  fixedBackground: true
+});
